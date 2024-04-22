f59b92a66954e6c03c617ac76fb030ef441f56ff
Periklis <perik76@gmail.com>
2024-04-22 15:14:59 +0300

    Header done

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import HeadLeftDiv from './HeadLeftDiv'
+import HeaderSearch from './HeaderSearch'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <HeadLeftDiv />
+            <HeaderSearch />
+        </header>
+    )
+}
