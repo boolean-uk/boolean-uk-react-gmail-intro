99ee215a70bc3c7cf0dc955d33e23e54483a7fe0
Periklis <perik76@gmail.com>
2024-04-22 16:03:11 +0300

    Left Bar done

--- src/LeftBar.jsx
+++ src/LeftBar.jsx
@@ -1,21 +1,10 @@
-export default LeftBar() {
+import LeftBarList from "./LeftBarList"
+
+export default function LeftBar() {
     return (
         <nav className="left-menu">
-            <ul className="inbox-list">
-                <li className="item active">
-                    <span className="label">Inbox</span>
-                    <span className="count">3</span>
-                </li>
-                <li className="item">
-                    <span className="label">Starred</span>
-                    <span className="count">2</span>
-                </li>
+            <LeftBarList />
 
-                <li className="item toggle">
-                    <label htmlFor="hide-read">Hide read</label>
-                    <input id="hide-read" type="checkbox" checked={false} />
-                </li>
-            </ul>
         </nav>
     )
 }
