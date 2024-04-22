e1835c57554fbaf6f382125995161b1ec891c7e4
Periklis <perik76@gmail.com>
2024-04-22 18:21:57 +0300

    Finished css components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './styles/App.css'
+import './styles/Header.css'
+import './styles/LeftMenu.css'
+import './styles/MailToolbar.css'
+import './styles/Title.css'
+import './styles/MailContent.css'
 
-import flaticonWelcomeImage from './assets/images/flaticon-welcome-image.png'
-import backArrow from './assets/icons/back-arrow.png'
-import downloadButton from './assets/icons/download-button.png'
-import rateStarButton from './assets/icons/rate-star-button.png'
-import rubbishButton from './assets/icons/rubbish-bin-delete-button.png'
+
 import Header from './Header'
 import LeftBar from './LeftBar'
 import MainContent from './MainContent'
