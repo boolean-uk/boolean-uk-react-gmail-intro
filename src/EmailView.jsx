12236ec856356d6c2dbce2f5f92721102c2d9eaa
timsakande <timiakande64@gmail.com>
2024-04-22 23:28:04 +0100

    update react

---
+++ src/EmailView.jsx
@@ -0,0 +1,17 @@
+import './styles/EmailView.css';
+import backArrow from '../assets/icons/back-arrow.png';
+import downloadButton from '../assets/icons/download-button.png';
+import rateStarButton from '../assets/icons/rate-star-button.png';
+import rubbishButton from '../assets/icons/rubbish-bin-delete-button.png';
+import flaticonWelcomeImage from '../assets/images/flaticon-welcome-image.png';
+
+function EmailView() {
+  return (
+    <main className="email-view">
+      {/* Email Toolbar */}
+      {/* Email Content */}
+    </main>
+  );
+}
+
+export default EmailView;
