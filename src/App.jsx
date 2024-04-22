6d13cdbf62143dabf4a68542cfa6bfc594aeb670
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-04-22 14:49:16 +0200

    split app css into seperate css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import './styles/App.css'
+import './styles/Header.css'
+import './styles/LeftMenu.css'
+import './styles/EmailToolbar.css'
+import './styles/EmailMain.css'
 import Header from './components/header/Header'
 import LeftMenu from './components/aside/LeftMenu'
 import MainContent from './components/main/MainContent'
