bbf8c8db448671950ea2501de6ceac6dcff18ab6
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-09-23 15:55:24 +0200

    Extension done

---
+++ src/components/EmailSender.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/EmailSender.css'
+
+const EmailSender = () => {
+  return (
+    <div className="email-sender-container">
+      <img src="../../images/write-email-component.png" alt="Email sender" />
+    </div>
+  );
+};
+
+export default EmailSender;
