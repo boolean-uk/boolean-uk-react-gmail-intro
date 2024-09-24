9627f9861b90d244ff0bb7d90965988aafc8394a
eyvmal <eyvind.malde@gmail.com>
2024-09-24 15:24:58 +0200

    add mui textfield

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import Header from "./components/Header.jsx";
 import Navigation from "./components/Navigation.jsx";
 import Toolbar from "./components/Toolbar.jsx";
 import EmailContent from "./components/EmailContent.jsx";
+import MuiTextField from "./components/MuiTextField.jsx";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Navigation />
-      <main className="email-view">
-        <Toolbar />
-        <EmailContent />
-      </main>
-    </div>
+      <div className="app">
+        <Header />
+        <Navigation />
+        <main className="email-view">
+          <Toolbar />
+          <EmailContent />
+          <MuiTextField />
+        </main>
+      </div>
   )
 }
 
