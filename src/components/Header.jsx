cd171f8efa3efc82ac3102c6fec7520064310e02
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-09-25 08:36:24 +0200

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import '../styles/Header.css'
 export function Header() {
     return(
         <header className="header">
-        <div className="left-menu">
+        <div className="left-menu-h">
           <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
             <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
           </svg>
