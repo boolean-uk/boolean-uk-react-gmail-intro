0666e53f7234fc3202ff19d1f7fb9815290ade1b
Julia <julia.r.lindgren@gmail.com>
2024-09-29 15:17:14 +0100

    Complete core

---
+++ src/components/EmailView.jsx
@@ -0,0 +1,23 @@
+import EmailToolbar from "./email/EmailToolbar"
+import EmailHeader from "./email/EmailHeader"
+import EmailBody from "./email/EmailBody"
+import EmailActions from "./email/EmailActions"
+import '../styles/Email.css'
+
+function EmailView() {
+    return (
+        <main className="email-view">
+        <EmailToolbar />
+        <article className="email-content">
+          <div className="title">
+            <h1>Welcome to Flaticon</h1>
+          </div>
+        <EmailHeader />
+        <EmailBody />
+        <EmailActions />
+        </article>
+      </main>
+    )
+}
+
+export default EmailView
